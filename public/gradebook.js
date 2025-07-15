8221bcbb22c24c92754c7f0e79b6322260a28e4e
AkshajP3 <aputtoju316@gmail.com>
2025-07-14 23:35:20 -0400

    Add server file and gradebook HTML

---
+++ public/gradebook.js
@@ -0,0 +1,12 @@
+function fetchGradeData() {
+    console.log("Fetching grade data...")
+    //Gets data from database
+}
+
+function populateGradebook(data){
+    console.log("Populating gradebook with data:", data);
+    //Puts data into gradebook 
+}
+
+const gradeData = fetchGradeData()
+populateGradebook(gradeData);
